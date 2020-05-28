2dadcc0a2eff1c0cc13c447e9ca04d120a3d3965
Elvis Payano <38874088+elvispayano@users.noreply.github.com>
2020-05-28 18:35:51 -0400

    Initial C++ Environment (#25)
    * Initial development of Matrix class and testing
    
    * Increasing Matrix Utility Capabilities and Coverage
    Adding testing for basic matrix operations
    Adding basic arithmetic operations

---
+++ trader/tests/test_matrix.cpp
@@ -0,0 +1,110 @@
+#include "matrix.h"
+#include "gtest/gtest.h"
+
+
+class MatrixTest : public ::testing::Test {
+protected:
+  void SetUp(void) override {
+    matA.Resize(3, 3);
+    matB.Resize(3, 3);
+
+    matA.Clear(2.0);
+    matB.Clear(4.0);
+  }
+
+  void TearDown(void) override {
+  }
+
+public:
+  dMatrix matA;
+  dMatrix matB;
+
+};
+
+TEST_F(MatrixTest, Initialize) {
+  dMatrix mat1;
+  EXPECT_EQ(mat1.Rows(), 0);
+  EXPECT_EQ(mat1.Cols(), 0);
+  EXPECT_DOUBLE_EQ(mat1(1, 2), 0.0);
+  EXPECT_DOUBLE_EQ(mat1(5, 2), 0.0);
+    
+    
+  dMatrix mat2(3, 3, 1.0);
+  EXPECT_EQ(mat2.Rows(), 3);
+  EXPECT_EQ(mat2.Cols(), 3);
+  EXPECT_DOUBLE_EQ(mat2(1, 2), 1.0);
+  EXPECT_DOUBLE_EQ(mat2(5, 2), 0.0);
+}
+
+TEST_F(MatrixTest, Reshaping) {
+  dMatrix mat1;
+  EXPECT_EQ(mat1.Rows(), 0);
+  EXPECT_EQ(mat1.Cols(), 0);
+  mat1.Resize(3, 3);
+  EXPECT_EQ(mat1.Rows(), 3);
+  EXPECT_EQ(mat1.Cols(), 3);
+  EXPECT_DOUBLE_EQ(mat1(1, 2), 0.0);
+  EXPECT_DOUBLE_EQ(mat1(5, 2), 0.0);
+  mat1.Clear(1.0);
+  EXPECT_DOUBLE_EQ(mat1(1, 2), 1.0);
+  EXPECT_DOUBLE_EQ(mat1(5, 2), 0.0);
+}
+
+TEST_F(MatrixTest, MatrixAddition) {
+  dMatrix matAdd(matA + matB);
+  EXPECT_DOUBLE_EQ(matAdd(2, 2), 6.0);
+
+  matA += matB;
+  EXPECT_DOUBLE_EQ(matA(2, 2), 6.0);
+}
+
+TEST_F(MatrixTest, ScalarAddition) {
+  dMatrix scaAdd(matA + 2.0);
+  EXPECT_DOUBLE_EQ(scaAdd(2, 2), 4.0);
+
+  matA += 2.0;
+  EXPECT_DOUBLE_EQ(matA(2, 2), 4.0);
+}
+
+TEST_F(MatrixTest, MatrixSubtraction) {
+  dMatrix mat(matA - matB);
+  EXPECT_DOUBLE_EQ(mat(2, 2), -2.0);
+
+  matA -= matB;
+  EXPECT_DOUBLE_EQ(matA(2, 2), -2.0);
+}
+
+TEST_F(MatrixTest, ScalarSubtraction) {
+  dMatrix mat(matA - 2.0);
+  EXPECT_DOUBLE_EQ(mat(2, 2), 0.0);
+
+  matA -= 2.0;
+  EXPECT_DOUBLE_EQ(matA(2, 2), 0.0);
+}
+
+TEST_F(MatrixTest, MatrixMultiplication) {
+  dMatrix mat1(3, 2, 2.0);
+  dMatrix mat2(2, 3, 4.0);
+
+  dMatrix mat(mat1 * mat2);
+  EXPECT_EQ(mat.Rows(), 3);
+  EXPECT_EQ(mat.Cols(), 3);
+
+  EXPECT_DOUBLE_EQ(mat(2, 2), 16);
+}
+
+TEST_F(MatrixTest, ScalarMultiplication) {
+  dMatrix mat(matA * 2);
+  EXPECT_DOUBLE_EQ(mat(2, 2), 4.0);
+
+  matA *= 2;
+  EXPECT_DOUBLE_EQ(matA(2, 2), 4.0);
+}
+
+TEST_F(MatrixTest, ScalarDivision) {
+  dMatrix mat(matA / 2);
+  EXPECT_DOUBLE_EQ(mat(2, 2), 1.0);
+
+  matA /= 2;
+  EXPECT_DOUBLE_EQ(matA(2, 2), 1.0);
+}
---
+++ trader/util/matrix.h
@@ -0,0 +1,303 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+template<typename T>
+class Matrix {
+public:
+  // Implementation & Functions
+  Matrix(void);
+  Matrix(size_t r, size_t c);
+  Matrix(size_t r, size_t c, T val);
+
+  void Clear(T = 0.0);
+  void Resize(size_t r, size_t c);
+  void Resize(size_t r, size_t c, T val);
+  size_t Rows(void) { return mr; }
+  size_t Cols(void) { return mc; }
+
+  Matrix<T> GetRow(size_t r);
+  Matrix<T> GetCol(size_t c);
+
+  T Determinant(void);
+  Matrix<T> SubMatrix(size_t r, size_t c);
+
+  // Operators
+  T& operator()(size_t r, size_t c);
+
+  // Matrix Operations
+  Matrix<T> operator+(Matrix<T>);
+  Matrix<T> operator-(Matrix<T>);
+  Matrix<T> operator*(Matrix<T>);
+  void operator+=(Matrix<T>);
+  void operator-=(Matrix<T>);
+
+  // Scalar Operations
+  Matrix<T> operator+(T);
+  Matrix<T> operator-(T);
+  Matrix<T> operator*(T);
+  Matrix<T> operator/(T);
+  void operator+=(T);
+  void operator-=(T);
+  void operator*=(T);
+  void operator/=(T);
+
+private:
+  size_t mr;
+  size_t mc;
+  T* mat;
+  T junk = 0;
+};
+
+//----------------------------------------------------------------
+// Implementation & Functions
+//----------------------------------------------------------------
+template<typename T>
+Matrix<T>::Matrix(void) {
+  mr = 0;
+  mc = 0;
+  mat = 0;
+  Resize(mr, mc);
+  Clear(static_cast<T>(0));
+}
+
+template<typename T>
+Matrix<T>::Matrix(size_t r, size_t c) {
+  mr = r;
+  mc = c;
+  mat = 0;
+  Resize(mr, mc);
+  Clear(static_cast<T>(0.0));
+}
+
+template<typename T>
+Matrix<T>::Matrix(size_t r, size_t c, T val) {
+  mr = r;
+  mc = c;
+  mat = 0;
+  Resize(mr, mc);
+  Clear(val);
+}
+
+template<typename T>
+void Matrix<T>::Clear(T init_val) {
+  for (unsigned int row_ind = 0; row_ind < mr; ++row_ind) {
+    for (unsigned int col_ind = 0; col_ind < mc; ++col_ind) {
+      this->operator()(row_ind, col_ind) = init_val;
+    }
+  }
+}
+
+template<typename T>
+void Matrix<T>::Resize(size_t r, size_t c) {
+  mr = r;
+  mc = c;
+  if (mat) delete mat; mat = 0;
+  mat = new T[mr * mc];
+  Clear(static_cast<T>(0.0));
+}
+
+template<typename T>
+void Matrix<T>::Resize(size_t r, size_t c, T val) {
+  mr = r;
+  mc = c;
+  if (mat) delete mat; mat = 0;
+  mat = new T[mr * mc];
+  Clear(val);
+}
+
+template<typename T>
+Matrix<T> Matrix<T>::GetRow(size_t r) {
+  Matrix<T> Output(1, mc, 0.0);
+  for (size_t c = 0; c < mc; ++c) {
+    Output(0, c) = this->operator()(r, c);
+  }
+  return Output;
+}
+
+template<typename T>
+Matrix<T> Matrix<T>::GetCol(size_t c) {
+  Matrix<T> Output(mr, 1, 0.0);
+  for (size_t r = 0; r < mr; ++r) {
+    Output(r, 0) = this->operator()(r,c);
+  }
+  return Output;
+}
+
+//----------------------------------------------------------------
+// Operator Assignment
+//----------------------------------------------------------------
+template<typename T>
+T& Matrix<T>::operator()(size_t r, size_t c) {
+  if (r >= mr) return junk;
+  if (c >= mc) return junk;
+  return mat[r + mc * c];
+}
+
+template<typename T>
+Matrix<T> Matrix<T>::operator+(Matrix<T> inp) {
+  Matrix<T> Output(mr, mc, 0.0);
+  if (mr != inp.Rows()) return Output;
+  if (mc != inp.Cols()) return Output;
+
+  for (size_t r = 0; r < mr; ++r) {
+    for (size_t c = 0; c < mc; ++c) {
+      Output(r, c) = this->operator()(r, c) + inp(r, c);
+    }
+  }
+  return Output;
+}
+
+template<typename T>
+Matrix<T> Matrix<T>::operator+(T scalar) {
+  Matrix<T> Output(mr, mr);
+  for (size_t r = 0; r < mr; ++r) {
+    for (size_t c = 0; c < mc; ++c) {
+      Output(r, c) = this->operator()(r, c) + scalar;
+    }
+  }
+  return Output;
+}
+
+template<typename T>
+void Matrix<T>::operator+=(Matrix<T> inp) {
+  if (mr != inp.Rows()) return;
+  if (mc != inp.Cols()) return;
+
+  for (size_t r = 0; r < mr; ++r) {
+    for (size_t c = 0; c < mc; ++c) {
+      this->operator()(r, c) += inp(r, c);
+    }
+  }
+}
+
+template<typename T>
+void Matrix<T>::operator+=(T scalar) {
+  for (size_t r = 0; r < mr; ++r) {
+    for (size_t c = 0; c < mc; ++c) {
+      this->operator()(r, c) += scalar;
+    }
+  }
+}
+
+template<typename T>
+Matrix<T> Matrix<T>::operator-(Matrix<T> inp) {
+  Matrix<T> Output(mr, mc, 0.0);
+  if (mr != inp.Rows()) return Output;
+  if (mc != inp.Cols()) return Output;
+
+  for (size_t r = 0; r < mr; ++r) {
+    for (size_t c = 0; c < mc; ++c) {
+      Output(r, c) = this->operator()(r, c) - inp(r, c);
+    }
+  }
+  return Output;
+}
+
+template<typename T>
+Matrix<T> Matrix<T>::operator-(T scalar) {
+  Matrix<T> Output(mr, mr);
+  for (size_t r = 0; r < mr; ++r) {
+    for (size_t c = 0; c < mc; ++c) {
+      Output(r, c) = this->operator()(r, c) - scalar;
+    }
+  }
+  return Output;
+}
+
+template<typename T>
+void Matrix<T>::operator-=(Matrix<T> inp) {
+  if (mr != inp.Rows()) return;
+  if (mc != inp.Cols()) return;
+
+  for (size_t r = 0; r < mr; ++r) {
+    for (size_t c = 0; c < mc; ++c) {
+      this->operator()(r, c) -= inp(r, c);
+    }
+  }
+}
+
+template<typename T>
+void Matrix<T>::operator-=(T scalar) {
+  for (size_t r = 0; r < mr; ++r) {
+    for (size_t c = 0; c < mc; ++c) {
+      this->operator()(r, c) -= scalar;
+    }
+  }
+}
+
+template<typename T>
+Matrix<T> Matrix<T>::operator*(T scalar) {
+  Matrix<T> Output(mr, mr, 0.0);
+  for (size_t r = 0; r < mr; ++r) {
+    for (size_t c = 0; c < mc; ++c) {
+      Output(r, c) = this->operator()(r, c) * scalar;
+    }
+  }
+  return Output;
+}
+
+template<typename T>
+Matrix<T> Matrix<T>::operator/(T scalar) {
+  Matrix<T> Output(mr, mr, 0.0);
+  for (size_t r = 0; r < mr; ++r) {
+    for (size_t c = 0; c < mc; ++c) {
+      Output(r, c) = this->operator()(r, c) / scalar;
+    }
+  }
+  return Output;
+}
+
+template<typename T>
+void Matrix<T>::operator*=(T scalar) {
+  for (size_t r = 0; r < mr; ++r) {
+    for (size_t c = 0; c < mc; ++c) {
+      this->operator()(r, c) *= scalar;
+    }
+  }
+}
+
+template<typename T>
+void Matrix<T>::operator/=(T scalar) {
+  for (size_t r = 0; r < mr; ++r) {
+    for (size_t c = 0; c < mc; ++c) {
+      this->operator()(r, c) /= scalar;
+    }
+  }
+}
+
+template<typename T>
+Matrix<T> Matrix<T>::operator*(Matrix<T> inp) {
+  Matrix<T> Output(mr, inp.Cols(), 0.0);
+  if (mc != inp.Rows()) return Output;
+  for (size_t r = 0; r < mr; ++r) {
+    for (size_t c = 0; c < inp.Cols(); ++c) {
+      T sum = 0;
+      for (size_t x = 0; x < mr; ++x) sum += GetRow(r)(0, x) * inp.GetCol(c)(x, 0);
+      Output(r, c) = sum;
+    }
+  }
+  return Output;
+}
+
+template<typename T>
+Matrix<T> Matrix<T>::SubMatrix(size_t r, size_t c) {
+  Matrix<T> Output(mr - 1, mc - 1, 0.0);
+  size_t sr = 0;
+  size_t sc = 0;
+  for (size_t row = 0; row < mr; ++row) {
+    for (size_t col = 0; col < mc; ++col) {
+      if (row == r || col == c) continue;
+      Output(sr++, sc++) = this->operator()(row, col);
+    }
+  }
+  return Output;
+}
+
+//----------------------------------------------------------------
+// Common Typedefs
+//----------------------------------------------------------------
+typedef Matrix<double> dMatrix;
+typedef Matrix<int>    iMatrix;
+typedef Matrix<float>  fMatrix;
+
+#endif // MATRIX_H
