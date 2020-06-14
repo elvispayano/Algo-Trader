428ef6807d1a8234ebcb5feac8a0451cdc395e78
Elvis Payano <elvispayano@outlook.com>
2020-06-14 18:39:27 -0400

    Initial IB API Integration

---
+++ trader/interfaces/ib_connect.cpp
@@ -0,0 +1,37 @@
+/*
+	Title:
+		IB Connect
+
+	Description:
+		This class is responsible for the platform side connection to the
+		TWS platform. The main object of this class is to interface with
+		the IB Wrapper and extend functionality as needed.
+
+	Author:
+		Elvis Payano
+*/
+
+// Interface Includes
+#include "ib_connect.h"
+
+/*
+	Constructor:	IBConnect
+	Inputs:				None (void)
+
+	Description:
+		Initialize class with default configuration
+*/
+IBConnect::IBConnect(void) {
+
+}
+
+/*
+	Destructor:		~IBConnect
+	Inputs:				None (void)
+
+	Description:
+		Initialize class with default configuration
+*/
+IBConnect::~IBConnect(void) {
+
+}
