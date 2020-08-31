d6ec81482852cd86c3695aafb6abd024d63fa55e
Elvis Payano <elvispayano@outlook.com>
2020-08-31 19:48:34 -0400

    Added documentation to Stock class

--- trader/util/stock.cpp
+++ trader/util/stock.cpp
@@ -1,32 +1,23 @@
-/*
-  Title:
-    Stock
-
-  Description:
-    This class is responsible for storing all the collected (and requested) data
-    about a single stock when it is requested from the broker.
-
-  Test:
-    test_stock.cc
-
-  Author:
-    Elvis Payano
-*/
+//! Stock
+//!
+//! The Stock class is responsible for storing all the collected (and requested)
+//! data about a single stock when it is requested from the broker.
+//!
+//! \author   Elvis Payano
+//! \date     31/08/2020
+//! \version  0.0.1
 
 // Utility Includes
 #include "stock.h"
 
-/*
-  Constructor:  Stock
-  Inputs:       None (void)
-
-  Description:
-    Setup default configuration for a given stock dataset
-*/
+//! @fn     Stock( void )
+//! @brief  Empty stock information list.
 Stock::Stock( void ) {
   reset();
 }
 
+//! @fn     void reset( void )
+//! @brief  Reset the class and clear any set values.
 void Stock::reset( void ) {
   // Bid
   recBid = false;
@@ -45,25 +36,35 @@ void Stock::reset( void ) {
   high    = 0.0f;
 }
 
+//! @fn     bool isComplete( void )
+//! @brief  Check if all expected responses have been received and recorded.
 bool Stock::isComplete( void ) {
   return recBid && recAsk && recLow && recHigh;
 }
 
+//! @fn     void setBid( float price)
+//! @brief  Record the holding bid price reponse.
 void Stock::setBid( float bid ) {
   this->bid = bid;
   recBid    = true;
 }
 
+//! @fn     void setAsk( float price )
+//! @brief  Record the holding ask price.
 void Stock::setAsk( float ask ) {
   this->ask = ask;
   recAsk    = true;
 }
 
+//! @fn     void  setLow( float price )
+//! @brief  Record the holding low price.
 void Stock::setLow( float low ) {
   this->low = low;
   recLow    = true;
 }
 
+//! @fn     void setHigh( float price )
+//! @brief  Record the holding high price.
 void Stock::setHigh( float high ) {
   this->high = high;
   recHigh    = true;
--- trader/util/stock.h
+++ trader/util/stock.h
@@ -1,17 +1,11 @@
-/*
-  Title:
-    Stock
-
-  Description:
-    This class is responsible for storing all the collected (and requested) data
-    about a single stock when it is requested from the broker.
-
-  Test:
-    test_stock.cc
-
-  Author:
-    Elvis Payano
-*/
+//! Stock
+//!
+//! The Stock class is responsible for storing all the collected (and requested)
+//! data about a single stock when it is requested from the broker.
+//!
+//! \author   Elvis Payano
+//! \date     31/08/2020
+//! \version  0.0.1
 
 #ifndef STOCK_H
 #define STOCK_H
@@ -25,33 +19,69 @@
 
 class Stock {
 public:
+  //! @fn     Stock( void )
+  //! @brief  Empty stock information list.
   Stock( void );
+
+  //! @fn     ~Stock( void )
+  //! @brief  Clear any allocated memory.
   ~Stock( void ) {}
 
-  // Status Functions
+  //! @fn     void reset( void )
+  //! @brief  Reset the class and clear any set values.
   void reset( void );
+
+  //! @fn     bool isComplete( void )
+  //! @brief  Check if all expected responses have been received and recorded.
   bool isComplete( void );
 
-  // Bid
-  void  setBid( float );
+  //! @fn     void setBid( float price)
+  //! @brief  Record the holding bid price reponse.
+  void setBid( float );
+
+  //! @fn     float getBid( void )
+  //! @brief  Return the recorded holding bid price.
   float getBid( void ) { return bid; }
 
-  // Ask
-  void  setAsk( float );
+  //! @fn     void setAsk( float price )
+  //! @brief  Record the holding ask price.
+  void setAsk( float );
+
+  //! @fn     float getAsk( void )
+  //! @brief  Return the recorded holding ask price.
   float getAsk( void ) { return ask; }
 
-  // Low
-  void  setLow( float );
+  //! @fn     void  setLow( float price )
+  //! @brief  Record the holding low price.
+  void setLow( float );
+
+  //! @fn     float getLow( void )
+  //! @brief  Return the recorded holding low price.
   float getLow( void ) { return low; }
 
-  // High
-  void  setHigh( float );
+  //! @fn     void setHigh( float price )
+  //! @brief  Record the holding high price.
+  void setHigh( float );
+
+  //! @fn     float getHigh( void )
+  //! @brief  Return the recorded holding high price.
   float getHigh( void ) { return high; }
 
-  void     setAction( Requests action ) { this->action = action; }
+  //! @fn     void setAction( Requests action )
+  //! @brief  Set the desired action that the broker should take with the
+  //!         holding.
+  void setAction( Requests action ) { this->action = action; }
+
+  //! @fn     Requests getAction( void )
+  //! @brief  Return the configured actions for the holding
   Requests getAction( void ) { return action; }
 
-  void        setTicker( std::string ticker ) { this->ticker = ticker; }
+  //! @fn     setTicker( string ticker )
+  //! @brief  Configure the holding for the specified ticker symbol
+  void setTicker( std::string ticker ) { this->ticker = ticker; }
+
+  //! @fn     getTicker( void )
+  //! @brief  Return the configured holding ticker symbol
   std::string getTicker( void ) { return ticker; }
 
 private:
