ecb29ad4ebf2d313601936fcccdde1331d877a94
Elvis Payano <elvispayano@outlook.com>
2020-08-30 22:33:32 -0400

    Added documentation to Broker Types

--- trader/util/broker_types.h
+++ trader/util/broker_types.h
@@ -1,15 +1,12 @@
-/*
-  Title:
-    Broker Types
-
-  Description:
-    A collection of various types that will be used by the trader platform to
-    communicate with the broker API. These custom types allow for the trader
-    platform to be agnostic of the API being utilized.
-
-  Author:
-    Elvis Payano
-*/
+//! Broker Types
+//!
+//! A collection of various types that will be used by the trader platform to
+//! communicate with the broker API. These custom types allow for the trader
+//! platforms to be agnostic of the APIT being utilized.
+//!
+//! \author   Elvis Payano
+//! \date     30/08/2020
+//! \version  0.0.1
 
 #ifndef BROKER_TYPES_H
 #define BROKER_TYPES_H
@@ -17,21 +14,23 @@
 // Standard Includes
 #include <string>
 
-/*
-  Enum:         Requests
-  Description:
-    Collection of potential requests that will be made to the broker API
-*/
+//! Requests
+//! Collection of potential requests that will be made to the broker API.
 enum class Requests { UPDATE, MARKET, LIMIT, STOP };
 
+//! ValueType
+//! Types of responses expected from the broker API
 enum class ValueType { HIGH, LOW, OPEN, CLOSE, BID, ASK, LAST, VOLUME, NONE };
 
+//! OrderConfig
+//! Message structure holding all the information required to buy or sell a
+//! holding
 struct OrderConfig {
-  std::string ticker;
-  Requests    request;
-  bool        purchase;
-  double      quantity;
-  double      price;
+  std::string ticker;    //! Holding ticker symbol
+  Requests    request;   //! Type of order to create
+  bool        purchase;  //! Purchase or sell holding (True=Buy; False=Sell)
+  double      quantity;  //! Purchase/Sell quantity
+  double      price;     //! Order price
 
   OrderConfig()
       : ticker( "" )
